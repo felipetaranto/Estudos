8ca25f7bf7b749e652b67b72ed599911711b1be2
Felipe Taranto <felipetaranto@gmail.com>
2021-03-04 14:24:41 -0300

    .

---
+++ Python/Curso/Aula02.py
@@ -0,0 +1,20 @@
+'''
+OPERADORES ARITIMÉTICOS
+
++ sinal de mais             adição/concatenação
+- sinal de menos            subtração
+* asterisco                 multiplicação
+/ barra                     divisão
+// barra dupla              divisão com resultado inteiro
+% porcento                  módulo (resto da divisão)
+** asteeisco duplo          exponenciação ou potenciação
+
+'''
+
+print('Soma de 2 com 2:', 2+2)
+print('Concatenação 2 com 2:', '2'+'2')
+print('Multiplicação 2 com 2:', 2*2)
+print('Repetição de "A" 5 vezes:', 'A' * 5)
+print('Divisão decimal 10 com 3:', 10/3)
+print('Divisão inteira de 10 com 3:', 10//3)
+print('Módulo (resto da divisão) entre 10 e 3:', 10 % 3)
